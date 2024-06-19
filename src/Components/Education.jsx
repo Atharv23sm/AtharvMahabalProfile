bf10e9d531dd892a8afb5ba4c0e1b2edacbd8ac4
Atharv Mahabal <122306441+Atharv23sm@users.noreply.github.com>
2024-06-19 20:38:38 +0530

    first commit

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,6 +1,6 @@
 function Education({ scroll }) {
     return (
-        <div className="w-full flex flex-col items-center gap-10 pb-12 md:pb-24" id="education">
+        <div className="w-full flex flex-col items-center gap-10 pb-20 md:pb-24" id="education">
             <div className={`w-full flex justify-center text-[13.5vw] opacity-40 font-phonk tracking-tight mt-32 md:mt-10 leading-none`}>EDUCATION</div>
             <div className={`w-[100%] md:w-[95%] md:backdrop-blur-sm md:bg-[#0008] md:-mt-[9.3vw] border-b-[1px] border-l-[1px] md:border-0 md:rounded-2xl flex flex-col gap-6 p-6 pt-0 md:p-12 md:text-lg duration-1000 ${scroll >= 1000 ? 'translate-y-0' : 'translate-y-[10vw]'}`}>
                 <div className="flex flex-col gap-8 md:gap-6">
